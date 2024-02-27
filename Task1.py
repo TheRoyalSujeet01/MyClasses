d5136a6abd1dfdca986281ff7eb99f535287ba58
TheRoyalSujeet01 <157133632+TheRoyalSujeet01@users.noreply.github.com>
2024-02-27 09:08:02 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,13 @@
+'''
+Task 1
+Runtime input:-
+print addition, multiplication, subtraction of two number.
+'''
+a = float(input("Enter Your First Number: "))
+b = float(input("Enter Your Second Number: "))
+c = a+b
+d = a*b
+e = a-b
+print("Sum of these two number is: ", c)
+print("Multiplication of these two number is: ", d)
+print("And Subtraction of these two number is:", e)
