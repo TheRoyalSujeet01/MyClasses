bc3d45624ee364dc8598a7c0c41b48cbe4f19893
TheRoyalSujeet01 <157133632+TheRoyalSujeet01@users.noreply.github.com>
2024-02-27 09:10:23 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,11 @@
+'''
+Task 2
+Print Your details:-
+name, age, email.
+'''
+name = "Sujeet Vishwakarma"
+age = 22
+email = "abcdefgh1234@gmail.com"
+print("Hello ", name )
+print("Your age is: ", age)
+print("And your email is:", email)
