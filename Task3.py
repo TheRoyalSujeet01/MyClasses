7b83bd2d0ad9e36804aa3055a8eed0281d3c8f07
TheRoyalSujeet01 <157133632+TheRoyalSujeet01@users.noreply.github.com>
2024-02-29 16:09:59 +0530

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,23 @@
+'''
+Take the input from user 2 numbers:
+and print the all operations using 7 operators that is given below:
+*Addition
+*Subtraction
+*Multiplication
+*Division
+*Modulus
+*Floor Division 
+*Exponential
+'''
+# Here I am takeing input from the user from two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Now Here I am going to perform arithmetic operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Floor Division:", num1 // num2)
+print("Exponential:", num1 ** num2)
