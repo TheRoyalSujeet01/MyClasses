0d0f34a30eb2a603ecc10914ed43f02bec64d473
TheRoyalSujeet01 <157133632+TheRoyalSujeet01@users.noreply.github.com>
2024-03-02 05:06:16 +0530

    Delete BasicMath.py

--- BasicMath.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Enter Your First Number: "))
-b = float(input("Enter Your Second Number: "))
-c = a+b
-d = a*b
-e = a-b
-print("Sum of these two number is: ", c)
-print("Multiplication of these two number is: ", d)
-print("And Subtraction of these two number is:", e)
