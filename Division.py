f5f72e858b85cdadcb3593dd98a5e596b3f4b468
TheRoyalSujeet01 <157133632+TheRoyalSujeet01@users.noreply.github.com>
2024-03-02 05:38:22 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your score: "))
+
+if score >= 70:
+    print("First division.")
+elif score >= 50:
+    print("Second division.")
+elif score >= 30:
+    print("Third division.")
+else:
+    print("Fail.")
